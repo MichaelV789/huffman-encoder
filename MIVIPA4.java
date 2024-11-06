024f007b90be6e8011140d9ee7d32918dd78d621
MichaelV789 <134097979+MichaelV789@users.noreply.github.com>
2024-11-05 23:38:28 -0600

    create node class

--- MIVIPA4.java
+++ MIVIPA4.java
@@ -1,6 +1,24 @@
 import java.util.*;
 import java.io.*;
 
+//node for max heap of charaters
+class Node implements Comparable<Node> {
+    char ch;
+    int freq;
+    Node left, right;
+
+    public Node(char ch, int freq) {
+        this.ch = ch;
+        this.freq = freq;
+        left = right = null;
+    }
+
+    @Override
+    public int compareTo(Node other) {
+        return this.freq - other.freq;
+    }
+}
+
 // priority queue to keep track of max heap
 
 // file parser
